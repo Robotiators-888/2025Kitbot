79d7edea9f9555783dc528ce19cfa8680e07a103
saphira <88745993+Axolotlxz@users.noreply.github.com>
2025-02-01 15:51:39 -0500

    Some Autos try to fix pathplanner
    pathplanner autos are not being regestered, autos made in vscode are.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.hal.HAL;
+import com.pathplanner.lib.commands.FollowPathCommand;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -38,6 +39,8 @@ public class Robot extends TimedRobot {
 
     // Used to track usage of Kitbot code, please do not remove.
     HAL.report(tResourceType.kResourceType_Framework, 10);
+
+    FollowPathCommand.warmupCommand().schedule();
   }
 
   /**
